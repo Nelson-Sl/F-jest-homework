dccf80b86b05fe4c4528e8bde7f3510369579a7f
Shen Liao <shen.liao@thoughtworks.com>
2020-09-16 19:21:25 +0800

    [Nelson Liao]: finish all jest practice

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,14 +1,26 @@
+import axios from "axios";
 import { register } from "../user";
+import { verifyPassword } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async () => {
     // TODO 19: add test here
+    axios.post.mockResolvedValueOnce({
+      data: {
+        name: "Nelson",
+      },
+    });
+    const result = register("fake username", "fake password");
+    await expect(result).resolves.toEqual({ name: "Nelson" });
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    verifyPassword("fake password").mockImplementation(() => false);
+    const result = register("fake username", "fake password");
+    await expect(result).rejects.toThrow("wrong username or password");
   });
 });
--- src/matchers/__test__/expect.calls.test.js
+++ src/matchers/__test__/expect.calls.test.js
@@ -7,6 +7,16 @@ test("TODO 11", () => {
   forEach([1, 2], mockCallback);
 
   // TODO 11: add assertion
+  // Test Method Call
+  expect(mockCallback).toHaveBeenCalled();
+  expect(mockCallback).toHaveBeenCalledTimes(2);
+  expect(mockCallback).toHaveBeenCalledWith(1);
+  expect(mockCallback).toHaveBeenCalledWith(2);
+  // Test Method Return
+  expect(mockCallback).toHaveReturned();
+  expect(mockCallback).toHaveReturnedTimes(2);
+  expect(mockCallback).toHaveReturnedWith(43);
+  expect(mockCallback).toHaveReturnedWith(44);
 });
 
 test("TODO 12", () => {
@@ -14,11 +24,11 @@ test("TODO 12", () => {
   mockFn
     .mockReturnValueOnce(42)
     .mockReturnValueOnce("string")
-    .mockReturnValue(true);
+    .mockReturnValue(true); // 表示剩下的都是true
 
   // TODO 12: to add "expected" value
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
+  expect(mockFn()).toBe(42);
+  expect(mockFn()).toBe("string");
+  expect(mockFn()).toBe(true);
+  expect(mockFn()).toBe(true);
 });
